37932704f3ae0ace5377354e04a72be10e1c6942
Ritvik <ritvik27@gmail.com>
2021-05-27 18:19:22 +0530

    fixed error

--- func.js
+++ func.js
@@ -5,7 +5,7 @@
 <h2>What Can JavaScript Do?</h2>
 
 <p id="demo">JavaScript can change HTML content.</p>
-
+<p>Hello new line added</p>
 <button type="button" onclick='document.getElementById("demo").innerHTML = "Hello JavaScript!"'>Click Me!</button>
 
 </body>
